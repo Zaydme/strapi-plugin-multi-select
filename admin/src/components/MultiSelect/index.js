9d51320503a4e5e16c41921bd3f2d48e9afaaf87
Yuku Takahashi <taka84u9@gmail.com>
2023-09-22 16:27:56 +0900

    Fix option splitting in MultiSelect to support colons in value
    Previously, an option like "foo:bar:baz" was split into "foo" for label and "bar" for value.
    This commit changes the splitting logic to treat it as "foo" for label and "bar:baz" for value.

--- admin/src/components/MultiSelect/index.js
+++ admin/src/components/MultiSelect/index.js
@@ -67,7 +67,9 @@ const MultiSelect = ({
   const possibleOptions = useMemo(() => {
     return (attribute['options'] || [])
       .map((option) => {
-        const [label, value] = [...option.split(':'), option]
+        // Split the option by the first colon. If no colon exists, use the entire string as the value.
+        const [label, ...els] = option.split(':')
+        const value = els.length ? els.join(':') : label
         if (!label || !value) return null
         return { label, value }
       })
